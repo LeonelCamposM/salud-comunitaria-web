a1c43071df143239a25724fa5e7fa4aad87bcc41
LeonelCamposM <leonelscamposm@gmail.com>
2023-11-24 14:32:44 -0600

    add offline resuorces to best perfeormance

--- docs/PsicoSociales/script.js
+++ docs/PsicoSociales/script.js
@@ -74,20 +74,17 @@ document.addEventListener("DOMContentLoaded", function () {
     var imagenSrc, descripcion;
     switch (value) {
       case "1":
-        imagenSrc =
-          "https://leonelcamposm.github.io/salud-comunitaria-web/imgs/autoestima_baja.png";
+        imagenSrc = "../imgs/autoestima_baja.png";
         descripcion =
           "Una autoestima baja puede hacer que el individuo busque refugio o consuelo en sustancias, viéndolas como una forma temporal de elevar su ánimo o de escapar de sentimientos de inferioridad o tristeza.";
         break;
       case "2":
-        imagenSrc =
-          "https://leonelcamposm.github.io/salud-comunitaria-web/imgs/autoestima_normal.png";
+        imagenSrc = "../imgs/autoestima_normal.png";
         descripcion =
           "Con una autoestima equilibrada, el individuo tiende a tener una percepción sana de sí mismo, reduciendo la necesidad de recurrir a sustancias como medio de escape o afirmación.";
         break;
       case "3":
-        imagenSrc =
-          "https://leonelcamposm.github.io/salud-comunitaria-web/imgs/autoestima_alta.png";
+        imagenSrc = "../imgs/autoestima_alta.png";
         descripcion =
           "Una autoestima muy alta puede hacer que el individuo sienta que puede experimentar con sustancias sin caer en la dependencia, pensando que tiene el control total sobre la situación.";
         break;
@@ -101,20 +98,17 @@ document.addEventListener("DOMContentLoaded", function () {
     var imagenSrc, descripcion;
     switch (value) {
       case "1":
-        imagenSrc =
-          "https://leonelcamposm.github.io/salud-comunitaria-web/imgs/confianza_baja.png"; // Reemplazar con la ruta de tu imagen para confianza baja
+        imagenSrc = "../imgs/confianza_baja.png"; // Reemplazar con la ruta de tu imagen para confianza baja
         descripcion =
           "Una baja confianza podría llevar al individuo a consumir sustancias como una forma de sentirse más seguro en situaciones sociales, pensando que las drogas pueden ayudarle a ´soltarse´ o encajar mejor.";
         break;
       case "2":
-        imagenSrc =
-          "https://leonelcamposm.github.io/salud-comunitaria-web/imgs/confianza_normal.png"; // Reemplazar con la ruta de tu imagen para confianza normal
+        imagenSrc = "../salud-comunitaria-web/imgs/confianza_normal.png"; // Reemplazar con la ruta de tu imagen para confianza normal
         descripcion =
           "Con un nivel de confianza medio, el individuo suele ser consciente de los riesgos y beneficios y tiene una probabilidad menor de ceder a presiones externas o de recurrir a sustancias para sentirse mejor.";
         break;
       case "3":
-        imagenSrc =
-          "https://leonelcamposm.github.io/salud-comunitaria-web/imgs/confianza_alta.png"; // Reemplazar con la ruta de tu imagen para confianza alta
+        imagenSrc = "../imgs/confianza_alta.png"; // Reemplazar con la ruta de tu imagen para confianza alta
         descripcion =
           "Una confianza excesiva podría llevar al individuo a pensar que puede controlar cualquier situación, incluido el consumo experimental de sustancias, creyendo que es capaz de manejarlo sin problemas.";
         break;
@@ -127,13 +121,11 @@ document.addEventListener("DOMContentLoaded", function () {
   function updateAmigosDrogas(checked) {
     var imagenSrc, descripcion;
     if (checked) {
-      imagenSrc =
-        "https://leonelcamposm.github.io/salud-comunitaria-web/imgs/amigos_usan_drogas.png";
+      imagenSrc = "../imgs/amigos_usan_drogas.png";
       descripcion =
         "Estar rodeado de amigos que consumen drogas aumenta significativamente la exposición y la posibilidad de probar o consumir regularmente, debido a la normalización del consumo en su entorno y la presión social.";
     } else {
-      imagenSrc =
-        "https://leonelcamposm.github.io/salud-comunitaria-web/imgs/sin_amigos_drogas.png";
+      imagenSrc = "../imgs/sin_amigos_drogas.png";
       descripcion =
         "Sin la presión o exposición a las drogas de parte de su círculo cercano, es menos probable que el individuo decida experimentar o consumir sustancias.";
     }
